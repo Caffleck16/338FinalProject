1adc59c2f80fe02c5a7a36820778ed23922f5155
carson-may <carson.may@ucalgary.ca>
2023-04-03 16:30:41 -0600

    finished dll

--- src/main/java/myLib/datastructures/linear/DLL.java
+++ src/main/java/myLib/datastructures/linear/DLL.java
@@ -1,5 +1,164 @@
 package main.java.myLib.datastructures.linear;
+import main.java.myLib.datastructures.nodes.*;
 
-public class DLL {
+
+public class DLL<T extends Comparable<T>>{
+    private DNode<T> head;
+    private DNode<T> tail;
+    private int size;
     
+    public DLL(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public DLL(DNode<T> head){
+        this.head = head;
+        this.tail = head;
+        this.size = 1;
+    }
+
+    public void insertHead(DNode<T> node) {
+        if (size==0) {
+            tail = node;
+        } else {
+            head.setPrevious(node);
+            node.setNext(head);
+        }
+        head = node;
+        size++;
+    }
+
+    public void insertTail(DNode<T> node) {
+        if (size==0) {
+            head = node;
+        } else {
+            tail.setNext(node);
+            node.setPrevious(tail);
+        }
+        tail = node;
+        size++;
+    }
+
+    public void insert(DNode<T> node, int position) {
+        if (position < 0 || position > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (position == 0) {
+            insertHead(node);
+        } else if (position == size) {
+            insertTail(node);
+        } else {
+            DNode<T> current = head;
+            for (int i = 0; i < position; i++) {
+                current = current.getNext();
+            }
+            node.setPrevious(current.getPrevious());
+            node.setNext(current);
+            current.getPrevious().setNext(node);
+            current.setPrevious(node);
+            size++;
+        }
+    }
+
+    public void sortedInsert(DNode<T> newNode) {
+        if(!isSorted()){
+            sort();
+        }
+
+        // If the list is empty, insert the new node as the head
+        if (head == null) {
+            insertHead(newNode);
+            return;
+        }
+    
+        // If the new node is less than the head node, insert it at the head
+        if (newNode.getData().compareTo(head.getData()) < 0) {
+            insertHead(newNode);
+            return;
+        }
+    
+        // If the new node is greater than the tail node, insert it at the tail
+        if (newNode.getData().compareTo(tail.getData()) > 0) {
+            insertTail(newNode);
+            return;
+        }
+    
+        // Search for the correct position to insert the new node
+        DNode<T> currNode = head.getNext();
+        while (currNode != null && newNode.getData().compareTo(currNode.getData()) > 0) {
+            currNode = currNode.getNext();
+        }
+    
+        // Insert the new node before the current node
+        DNode<T> prevNode = currNode.getPrevious();
+        newNode.setPrevious(prevNode);
+        newNode.setNext(currNode);
+        currNode.setPrevious(newNode);
+        prevNode.setNext(newNode);
+    
+        size++;
+    }
+
+    public boolean isSorted() {
+        if (head == null || head.getNext() == null) {
+            // If the list is empty or has only one element, it is considered sorted
+            return true;
+        }
+        DNode<T> current = head;
+        while (current.getNext() != null) {
+            if (current.getData().compareTo(current.getNext().getData()) > 0) {
+                // If the current node is greater than the next node, the list is not sorted
+                return false;
+            }
+            current = current.getNext();
+        }
+        // If we reach the end of the list without finding an out-of-order pair, it is sorted
+        return true;
+    }
+
+    private void sort(){
+        if (head == null || head.getNext() == null) {
+            return; // The list is already sorted
+        }
+    
+        DNode<T> current = head.getNext();
+        while (current != null) {
+            T data = current.getData();
+            DNode<T> prev = current.getPrevious();
+            while (prev != null && prev.getData().compareTo(data) > 0) {
+                prev.getNext().setData(prev.getData());
+                prev = prev.getPrevious();
+            }
+            if (prev == null) {
+                head.setData(data);
+            } else {
+                prev.getNext().setData(data);
+            }
+            current = current.getNext();
+        }
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    
+    public void print() {
+        System.out.println("List Length: " + size);
+        System.out.println("Sorted Status: " + isSorted());
+        System.out.println("List Contents:");
+        if(head == null){
+            System.out.println("List is empty");
+        }
+        var curNode = head;
+        int i = 0;
+        while(curNode != null){
+            System.out.println("Node " + i + ": " + curNode.getData());
+            curNode = curNode.getNext();
+            i++;
+        }
+    }
 }
--- src/test/DataStructuresTest.java
+++ src/test/DataStructuresTest.java
@@ -1,23 +1,40 @@
 package test;
 import main.java.myLib.datastructures.linear.*;
-import main.java.myLib.datastructures.nodes.Node;
+import main.java.myLib.datastructures.nodes.*;
+import java.util.*;
 
 public class DataStructuresTest {
+    private static ArrayList<Node<Integer>> nodeList = new ArrayList<>();
+    private static ArrayList<DNode<Integer>> dNodeList = new ArrayList<>();
     public static void main(String args[]){
-        SLLTest();
+        nodeList.add(new Node<>(10));
+        nodeList.add(new Node<>(50));
+        nodeList.add(new Node<>(30));
+        nodeList.add(new Node<>(40));
+        nodeList.add(new Node<>(20));
+        dNodeList.add(new DNode<>(10));
+        dNodeList.add(new DNode<>(50));
+        dNodeList.add(new DNode<>(30));
+        dNodeList.add(new DNode<>(40));
+        dNodeList.add(new DNode<>(20));
+        DLLTest();
     }
 
     public static void SLLTest(){
-        Node<Integer> node1 = new Node<>(10);
-        Node<Integer> node2 = new Node<>(50);
-        Node<Integer> node3 = new Node<>(30);
-        Node<Integer> node4 = new Node<>(40);
-        Node<Integer> node5 = new Node<>(20);
-        SLL<Integer> sll = new SLL<>(node1);
-        sll.insertHead(node2);
-        sll.insertHead(node3);
-        sll.insertHead(node4);
-        sll.sortedInsert(node5);
+        SLL<Integer> sll = new SLL<>(nodeList.get(0));
+        sll.insertHead(nodeList.get(1));
+        sll.insertHead(nodeList.get(2));
+        sll.insertHead(nodeList.get(3));
+        sll.sortedInsert(nodeList.get(4));
         sll.print();
+    }
+
+    public static void DLLTest(){
+        DLL<Integer> dll = new DLL<>(dNodeList.get(0));
+        dll.insertHead(dNodeList.get(1));
+        dll.insertHead(dNodeList.get(2));
+        dll.insertHead(dNodeList.get(3));
+        dll.insert(dNodeList.get(4), 0);
+        dll.print();
     }
 }
